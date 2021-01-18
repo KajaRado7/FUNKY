b8676714336b850655bee40e9b763b9508330992
Kaja Radošević <73023084+KajaRado7@users.noreply.github.com>
2021-01-18 14:54:03 +0100

    Događaji za Južnu,Istočnu i Gorsku hrv

---
+++ src/views/Gorska.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="row">
+    <div class="col-1"></div>
+    <div class="col-10">
+      <h2>Sorry,there are no events available yet ...</h2>
+      <font-awesome-icon icon="frown" color=" #f5b85c" size="2x" />
+    </div>
+    <div class="col-1"></div>
+    <footer id="footer"></footer>
+  </div>
+</template>
+
+<style scoped>
+h2 {
+  color: #f5b85c;
+}
+#footer {
+  width: 100%;
+  height: 100px;
+}
+</style>
---
+++ src/views/Istocna.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="row">
+    <div class="col-1"></div>
+    <div class="col-10">
+      <dogadaji-card v-for="card in cards" :key="card" :info="card" />
+      <!-- kada stavimo : onda ovaj = prima js  ako stavim samo info prenjet ce se string -->
+    </div>
+    <div class="col-1"></div>
+    <footer id="footer"></footer>
+  </div>
+
+  <!--<template>
+  <div class="container" style="max-width: 500px;">
+    <div id="cards">
+      <regije-card v-for="card in cards" :key="card" :info="card" />
+    </div>
+    <footer id="footer"></footer>
+  </div>
+</template> -->
+</template>
+
+<script>
+import DogadajiCard from '@/components/DogadajiCard.vue';
+
+let cards = [];
+
+cards = [
+  {
+    img: require('@/assets/book_club_outdoor.jpg'),
+    naslov: 'Bookworm',
+    heart: false,
+  },
+  {
+    img: require('@/assets/concert_jazz.jpg'),
+    naslov: 'Jazz vibes',
+    heart: false,
+  },
+];
+
+export default {
+  name: 'istocna',
+  data: function() {
+    return {
+      cards: cards,
+    };
+  },
+  components: {
+    DogadajiCard,
+  },
+};
+</script>
+
+<style scoped>
+#footer {
+  width: 100%;
+  height: 100px;
+}
+</style>
