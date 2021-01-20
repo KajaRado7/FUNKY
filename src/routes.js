4c23cafd642d2e03279429799c2a8bb9104174cc
Dejana Pivac <dpivac@student.unipu.hr>
2021-01-20 18:57:47 +0100

    prvi commit

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,11 @@
 import RegijeCard from '@/components/RegijeCard.vue';
 import singleRegija from '@/components/singleRegija.vue';
 
-export default[
+/*export default[
     { path: '/', component: RegijeCard},
     { path: '/regije/:id', component: singleRegija}
+] */
+
+export const routes = [
+    { path: '/regije/:id', component: Regije }
 ]
